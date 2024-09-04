b23a8558cc9c9d172af8e1b3f9bfaf54fbec3581
Zhair Gomez <166151278+ZhairG@users.noreply.github.com>
2024-09-03 19:28:03 -0500

    Plantilla
    Se crea la plantilla del codigo que se va usar para crear el juego

---
+++ Game.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Game {   
+    
+    public void tablero() {
+            String[][] lista= {{" "," "," "," "," "," "," "," "," "},{" "," "," "," "," "," "," "," "," "},{" "," "," "," "," "," "," "," "," "},{" "," "," "," "," "," "," "," "," "},{" "," "," "," "," "," "," "," "," "},
+            {" "," "," "," "," "," "," "," "," "},{" "," "," "," "," "," "," "," "," "},{" "," "," "," "," "," "," "," "," "},{" "," "," "," "," "," "," "," "," "}};
+            System.out.println("Jugador 1");
+            System.out.println("     A     B     C     D     E     F     G     H     I");
+            System.out.println("  -------------------------------------------------------");
+            for (int fila = 0; fila < lista.length; fila++) {
+            System.out.print(fila+" ");
+            for (int columna = 0; columna < lista[fila].length; columna++) {
+                 System.out.print("|  "+lista[fila][columna]+"  ");
+            }
+            System.out.println("|");
+            System.out.println("  -------------------------------------------------------");
+            }
+    }
+    // Metodo para posicionar los barcos
+    public void pos() {
+
+    }
+    // Metodo para posicionar los tiros.
+    public void tiros() {
+
+    }
+    // Función principal
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        // Usamos la clase BufferedReader para leer cadenas de texto
+        Game Game = new Game(); // Se crea un objeto
+        Game.tablero();
+    }
+}
